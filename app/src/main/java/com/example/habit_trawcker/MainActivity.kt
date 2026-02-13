32bd07350d4b90027091d3e7692e36b99c6f9ad7
greedygoblin <greedygobling@gmail.com>
2026-02-13 16:05:46 +0100

    logout for testing

--- app/src/main/java/com/example/habit_trawcker/MainActivity.kt
+++ app/src/main/java/com/example/habit_trawcker/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.habit_trawcker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.habit_trawcker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val authRepo = AuthRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signOutButton.setOnClickListener {
+            authRepo.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+
+            // user kan inte komma tillbacka här
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intent)
         }
     }
 }
